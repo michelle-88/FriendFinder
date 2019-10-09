3e02c91555ed04dd7ec77bbeed3c264969265866
Michelle M <53539385+michelle-88@users.noreply.github.com>
2019-10-08 23:18:15 -0400

    added logic in apiRoutes.js file that will determine each user's most compatible friend. the answers submitted by the user via the survey form are accessed via the req.body object. the absolute difference between the user's scores and the scores of each person in the friends array is then calculated in a for loop. these differences are then added together for each friend in the array and the 'total difference' is then saved as a new property in each friend object. the value of this new property is then compared for each object in the friends array and the friend object with the smallest total difference is then sent to the user in the response.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,7 +10,54 @@ module.exports = function(app) {
 
         var newFriend = req.body;
 
-        return res.json(friends[0])
+        console.log(newFriend);
+
+        var newFriendScores = req.body.scores;      
+
+        const add = (a, b) => a + b;        
+
+        for(var i = 0; i < friends.length; i++) {
+            let friendScores = friends[i].scores;
+
+            let diffArray = [];
+
+            for(var j = 0; j < friendScores.length; j++) {
+                let diff = Math.abs(friendScores[j] - parseInt(newFriendScores[j]));
+                diffArray.push(diff);
+            }
+
+            let totalDifference = diffArray.reduce(add)
+
+            friends[i].totalDifference = totalDifference;
+        }
+
+        console.log(friends);
+        
+        var findMatchArr = []
+
+        for(var i = 0; i < friends.length; i++) {
+            findMatchArr.push(friends[i].totalDifference);
+        }
+        
+        var index = 0; 
+        var value = findMatchArr[0];
+        for(var i = 1; i < findMatchArr.length; i++) {
+            if(findMatchArr[i] < value) {
+                value = findMatchArr[i];
+                index = i;
+            }
+        }
+        
+        console.log(value, index)
+
+        res.json(friends[index]);
+
+        friends.push(newFriend);
+
+
+
+
+        // return res.json(friends[0])
         // write logic here to compare new friend to friends in array
         // 2 for loops - one to loop thru each user and then another to loop thru each user's scores array property
         // while looping thru the scores array, find difference between user and new user's scores
